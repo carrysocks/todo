656dc59e8ef4d75a79a54786ab1ed9f010e8a2ce
carrysocks <null>
2020-03-12 14:25:29 +0000

    todo commit

--- todoList.js
+++ todoList.js
@@ -2,6 +2,8 @@ const todoForm = document.querySelector(".js-todoForm"),
 	  todoList = document.querySelector(".js-todoList"),
 	  todoInput = todoForm.querySelector(".js-todo-input");
 
+const todo_list = [];
+
 function init(){
 	listCheck();
 	todoForm.addEventListener("submit",handleSubmit);
@@ -19,18 +21,26 @@ function paintTodo(text){
 	const li = document.createElement("li");
 	const span = document.createElement("span");
 	const button = document.createElement("button");
+	const newId = todo_list.length+1;
 	span.innerText = text;
 	button.innerHTML = "X";
+	li.id =newId;
 	li.appendChild(span);
 	li.appendChild(button);
 	todoList.appendChild(li);
+	todoObj = {
+		text:text,
+		id:newId
+	};
+	todo_list.push(todoObj);
 }
 
 function handleSubmit(event){
 	event.preventDefault();
 	let currentValue = todoInput.value;
-	paintTodo();
-	pushLS(currentValue);
+	paintTodo(currentValue);
+	//pushLS()
+	todoInput.value ="";
 }
 
 init();

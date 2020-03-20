026eb4e3f6550201328c14120bc7527fe89137fb
carrysocks <null>
2020-03-20 11:31:14 +0000

    finish function

--- greeting.js
+++ greeting.js
@@ -2,6 +2,8 @@ const greetingForm = document.querySelector(".js-greeting"),
 	  greetingInput = greetingForm.querySelector(".js-greeting-input"),
 	  name = document.querySelector(".js-name");
 
+const todoFormList = document.querySelector(".js-todoForm");
+
 function init(){
 	getName();
 }
@@ -9,11 +11,26 @@ function init(){
 function getName(){
 	const localName = localStorage.getItem("NAME");
 	if(localName === null){
-		
+		greetingForm.addEventListener("submit",handleSubmitName);
 	}
 	else{
-		
+		showName(localName);
 	}
+}
+
+function handleSubmitName(event){
+	event.preventDefault();
+	const userName = greetingInput.value;
+	localStorage.setItem("NAME",userName);
+	greetingInput.value = "";
+	showName(userName);
+}
+
+function showName(localname){
+	greetingForm.classList.add("hide");
+	name.innerHTML = `반갑습니다. ${localname}님`;
+	name.classList.replace("hide","show");
+	todoFormList.classList.replace("hide","show");
 }
 
 init();
